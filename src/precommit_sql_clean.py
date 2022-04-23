a4b22ecf92de53a57d1548aca7cd38a7f40ae5e1
Chad Upjohn <chad.upjohn.-nd@disneystreaming.com>
2022-04-23 11:54:26 -0500

    adding hook check

--- src/precommit_sql_clean.py
+++ src/precommit_sql_clean.py
@@ -2,8 +2,6 @@ import re
 
 
 def remove_endlines(file_content: str) -> str:
-    # with open(file_name) as f:
-    #     file_thing = f.read()
 
     file_lines = file_content.splitlines()
 
@@ -19,15 +17,16 @@ def remove_endlines(file_content: str) -> str:
     return "\n".join(file_lines)
 
 
-if __name__ == "__main__":
-    file_name = "./zz_notes/what.sql"
-    _ = remove_endlines(file_name=file_name)
-# .fs.create_file(file_path, contents = 'test')
-# def my_fakefs_test(fs):
-#     # "fs" is the reference to the fake file system
-#         fs.create_file('/var/data/xx1.txt')
-#             assert os.path.exists('/var/data/xx1.txt')
-#             pytest
-#             plugin: pyfakefs
-#             pip install pyfakefs
-#
+def hook_check_file(file_name: str) -> int:
+    with open(file_name) as f:
+        file_content = f.read()
+
+    check_endlines = remove_endlines(file_content)
+
+    if file_content == check_endlines:
+        return 1
+
+    with open(file_name, "w") as f:
+        f.write(check_endlines)
+
+    return 0
--- tests/test_line_removal.py
+++ tests/test_line_removal.py
@@ -1,19 +1,16 @@
-from precommit_sql_clean import remove_endlines
+import pytest
+
+from precommit_sql_clean import hook_check_file, remove_endlines
 
 
-def test_basic_file_no_extra_lines(fs):
-    # "fs" is the reference to the fake file system
-    # file_name = "/var/data/xx1.txt"
+def test_basic_file_no_extra_lines():
     file_content = "select *"
-    # fs.create_file(file_name, contents=file_content)
     result = remove_endlines(file_content)
-    # with open(file_name) as f:
-    #     result = f.read()
 
     assert file_content == result
 
 
-def test_basic_file_with_extra_lines(fs):
+def test_basic_file_with_extra_lines():
     file_content_base = "select *"
     file_content = f"{file_content_base}\n  \n"
     result = remove_endlines(file_content)
@@ -21,14 +18,46 @@ def test_basic_file_with_extra_lines(fs):
     assert file_content_base == result
 
 
-def test_empty_file_with_extra_lines(fs):
+def test_empty_file_with_extra_lines():
     file_content = ""
     result = remove_endlines(file_content)
     assert file_content == result
 
 
-def test_blank_lines_file_with_extra_lines(fs):
+def test_blank_lines_file_with_extra_lines():
     file_content = "    "
     result = remove_endlines(file_content)
 
     assert "" == result
+
+
+def test_basic_file_no_extra_lines_pass(fs):
+    file_name = "/var/data/xx1.txt"
+    file_content = "select *"
+    fs.create_file(file_name, contents=file_content)
+    hook_result = hook_check_file(file_name)
+    with open(file_name) as f:
+        file_result = f.read()
+
+    with pytest.assume:
+        assert file_result == file_content
+
+    with pytest.assume:
+        assert hook_result == 1
+
+
+def test_basic_file_with_extra_lines_fail(fs):
+    file_name = "/var/data/xx1.txt"
+    file_content_base = "select *"
+    file_content = f"{file_content_base}\n  \n"
+    fs.create_file(file_name, contents=file_content)
+    hook_result = hook_check_file(file_name)
+
+    with open(file_name) as f:
+        file_result = f.read()
+
+    with pytest.assume:
+        assert file_result == file_content_base
+
+    with pytest.assume:
+        assert hook_result == 0
